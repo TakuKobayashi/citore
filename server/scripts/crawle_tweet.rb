0021391b98f52e9323e07dd51e390cbb0db6e846
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-13 01:01:33 +0900

    twitter検索できました

--- server/scripts/crawle_tweet.rb
+++ server/scripts/crawle_tweet.rb
@@ -1,1 +1,13 @@
-puts "Hello world"
+apiconfig = YAML.load(File.open("config/apiconfig.yml"))
+
+client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = apiconfig["twitter"]["consumer_key"]
+    config.consumer_secret     = apiconfig["twitter"]["consumer_secret"]
+    config.access_token        = apiconfig["twitter"]["access_token_key"]
+    config.access_token_secret = apiconfig["twitter"]["access_token_secret"]
+end
+
+last_id = nil
+client.search('エロく聞こえる言葉 -rt', :lang => "ja", :count => 100, :max_id => last_id).map do |status|
+  puts "#{status.to_json}"
+end

37fa96c9db713dde3b19c424ca6b1a0a97207aa2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-13 06:16:29 +0900

    一応Cabochaもいれておいた

--- server/scripts/crawle_tweet.rb
+++ server/scripts/crawle_tweet.rb
@@ -8,10 +8,13 @@ client = Twitter::REST::Client.new do |config|
 end
 
 natto = Natto::MeCab.new
+parser = CaboCha::Parser.new
 
 last_id = nil
 client.search('エロく聞こえる言葉 -rt', :lang => "ja", :count => 100, :max_id => last_id).map do |status|
   natto.parse(status.text) do |n|
     puts "#{n.surface}\t#{n.feature}"
   end
+  tree = parser.parse(status.text)
+  puts tree.toString(CaboCha::FORMAT_TREE)
 end

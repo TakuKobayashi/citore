2ce76b36261a1ed1fb27136634b05bf647744bba
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-13 00:24:42 +0900

    エロく聞こえるtwitter_stream_apiできた

--- server/scripts/stream_tweet.rb
+++ server/scripts/stream_tweet.rb
@@ -1,1 +1,14 @@
-puts "Hello world2"
+apiconfig = YAML.load(File.open("config/apiconfig.yml"))
+TweetStream.configure do |config|
+  config.consumer_key       = apiconfig["twitter"]["consumer_key"]
+  config.consumer_secret    = apiconfig["twitter"]["consumer_secret"]
+  config.oauth_token        = apiconfig["twitter"]["access_token_key"]
+  config.oauth_token_secret = apiconfig["twitter"]["access_token_secret"]
+  config.auth_method        = :oauth
+end
+
+client = TweetStream::Client.new
+
+client.track('エロく聞こえる言葉') do |status|
+  puts "#{status.user.screen_name}: #{status.text}"
+end

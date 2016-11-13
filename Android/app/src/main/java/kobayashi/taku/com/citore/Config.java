73cac6ccee0b1242a60b5ac0b18681e032439051
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-13 15:05:38 +0900

    通常のAndroidでHttpRequest

---
+++ Android/app/src/main/java/kobayashi/taku/com/citore/Config.java
@@ -0,0 +1,7 @@
+package kobayashi.taku.com.citore;
+
+public class Config {
+	public static final String TAG = "citore";
+	public static final String ROOT_URL = "http://taptappun.cloudapp.net/";
+	public static final String WS_ROOT_URL = "ws://taptappun.cloudapp.net:3000/";
+}
--- Android/app/src/main/java/kobayashi/taku/com/citore/MainActivity.java
+++ Android/app/src/main/java/kobayashi/taku/com/citore/MainActivity.java
@@ -1,7 +1,12 @@
 package kobayashi.taku.com.citore;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HashMap<String, Object> params = new HashMap<String, Object>();
+        params.put("text", "オナモミ");
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("http");
+        builder.authority("taptappun.cloudapp.net");
+        builder.path("/tweet_voice/search");
+        builder.appendQueryParameter("text", "オナモミ");
+        JsonRequest req = new JsonRequest();
+        req.addCallback(new JsonRequest.ResponseCallback() {
+            @Override
+            public void onSuccess(String url, String body) {
+                Log.d(Config.TAG, "url:" + url + " body:" + body);
+            }
+        });
+        req.execute(builder.toString());
     }
 }

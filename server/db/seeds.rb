44917fe168c3090b636f3e43715c75c7243ef51b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-22 03:26:35 +0900

    dynamodbにデータを入れていくスクリプトを作成

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -14,12 +14,22 @@ en = File.read(Rails.root.to_s + "/scripts/dictionary/pn_en.dic")
 ens = en.split("\n")
 ens.each do |e|
   es = e.split(":")
-  dic = EmotionalWordDictionary.new
-  dic.word = es[0]
-  dic.reading = es[0]
-  dic.part = es[1]
-  dic.score = es[2]
-  dics << dic
+  dic = EmotionalWordDynamo.new({
+    word: es[0],
+    part: es[1],
+    info: {
+      reading: es[0],
+      score: es[2].to_f
+    }
+  })
+  dic.save!
+
+#  dic = EmotionalWordDictionary.new
+#  dic.word = es[0]
+#  dic.reading = es[0]
+#  dic.part = es[1]
+#  dic.score = es[2]
+#  dics << dic
   puts dic.to_json if dic.part.nil?
 end
 
@@ -28,13 +38,22 @@ ja = File.read(Rails.root.to_s + "/scripts/dictionary/pn_ja.dic")
 jas = ja.split("\r\n")
 jas.each do |j|
   js = j.split(":")
-  dic = EmotionalWordDictionary.new
-  dic.word = js[0]
-  dic.reading = js[1]
-  dic.part = parts[js[2]]
-  dic.score = js[3]
-  dics << dic
+  dic = EmotionalWordDynamo.new({
+    word: js[0],
+    part: parts[js[2]],
+    info: {
+      reading: js[1],
+      score: js[3].to_f
+    }
+  })
+  dic.save!
+#  dic = EmotionalWordDictionary.new
+#  dic.word = js[0]
+#  dic.reading = js[1]
+#  dic.part = parts[js[2]]
+#  dic.score = js[3]
+#  dics << dic
   puts dic.to_json if dic.part.nil?
 end
 
-EmotionalWordDictionary.import(dics)
+# EmotionalWordDictionary.import(dics)

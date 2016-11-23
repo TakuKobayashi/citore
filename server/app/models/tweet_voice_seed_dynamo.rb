9d5ac46ef02481b0d67405ca62c97601f754dba8
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-11-23 15:45:26 +0900

    tweet voice dynamoの追加

--- server/app/models/tweet_voice_seed_dynamo.rb
+++ server/app/models/tweet_voice_seed_dynamo.rb
@@ -1,2 +1,7 @@
-class TweetVoiceSeedDynamo < ApplicationRecord
+class TweetVoiceSeedDynamo
+  include Aws::Record
+
+  string_attr :key,  hash_key: true
+  string_attr :reading, range_key: true
+  map_attr    :info
 end
--- server/db/migrate/20161122150352_create_tweet_voice_seed_dynamos.rb
+++ server/db/migrate/20161122150352_create_tweet_voice_seed_dynamos.rb
@@ -1,8 +1,18 @@
 class CreateTweetVoiceSeedDynamos < ActiveRecord::Migration[5.0]
-  def change
-    create_table :tweet_voice_seed_dynamos do |t|
+  def up
+    return if TweetVoiceSeedDynamo.table_exists?
+    migration = Aws::Record::TableMigration.new(TweetVoiceSeedDynamo)
+    migration.create!(
+      provisioned_throughput: {
+        read_capacity_units: 5,
+        write_capacity_units: 5
+      }
+    )
+    migration.wait_until_available
+  end
 
-      t.timestamps
-    end
+  def down
+    migration = Aws::Record::TableMigration.new(TweetVoiceSeedDynamo)
+    migration.delete!
   end
 end

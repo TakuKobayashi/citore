6648068ff6c323d812621a2d7401bcf218757f52
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-12-04 01:59:29 +0900

    ツイッターのデータをただひたすら突っ込んでいくバッチ完成

--- server/scripts/stream_tweet.rb
+++ server/scripts/stream_tweet.rb
@@ -7,8 +7,66 @@ TweetStream.configure do |config|
   config.auth_method        = :oauth
 end
 
-client = TweetStream::Client.new
+natto = Natto::MeCab.new
 
-client.track('エロく聞こえる言葉') do |status|
-  puts "#{status.user.screen_name}: #{status.text}"
+client = TweetStream::Client.new
+client.sample do |status|
+  next if status.lang != "ja"
+  sanitaized_word = TweetVoiceSeedDynamo.sanitized(status.text)
+  without_url_tweet, urls = TweetVoiceSeedDynamo.separate_urls(sanitaized_word)
+  without_kaomoji_tweet, kaomojis = TweetVoiceSeedDynamo.separate_kaomoji(without_url_tweet)
+  tweet_words = TweetVoiceSeedDynamo.bracket_split(without_kaomoji_tweet)
+  tweet_words = tweet_words.map{|t| TweetVoiceSeedDynamo.delete_symbols(t) }
+
+  begin
+    tweet = TwitterWord.create!(
+      twitter_user_id: status.user.id.to_s,
+      twitter_user_name: status.user.screen_name.to_s,
+      twitter_tweet_id: status.id.to_s,
+      tweet: without_url_tweet,
+      csv_url: urls.join(","),
+      tweet_created_at: status.created_at
+    )
+
+    import_words = []
+    appear_imports = {}
+    kaomojis.each do |kaomoji|
+      appears = appear_imports[kaomoji]
+      if appears.present?
+        count = appears[:appear_count]
+      end
+      appear_imports[kaomoji] = {word: kaomoji, part: EmotionalWordDynamo::KAOMOJI_PART, appear_count: count.to_i + 1}
+    end
+
+    tweet_words.each do |tweet_word|
+      natto.parse(tweet_word) do |n|
+        next if n.surface.blank?
+        features = n.feature.split(",")
+        part = EmotionalWordDynamo::PARTS[features[0]]
+        next if part.blank? || part == "av"
+        word = features[6]
+        if word.blank? || word == "*"
+          word = n.surface
+        end
+        appears = appear_imports[word]
+        if appears.present?
+          count = appears[:appear_count]
+        end
+        appear_imports[word] = {word: word, part: part, appear_count: count.to_i + 1}
+      end
+    end
+
+    appear_words = []
+    appear_imports.each do |word, hash|
+      appear_word = TweetAppearWord.new(hash)
+      import_words << appear_word
+    end
+    TweetAppearWord.import(import_words, on_duplicate_key_update: "appear_count = appear_count + VALUES(appear_count), `updated_at`=VALUES(`updated_at`)")
+    ids = TweetAppearWord.where(word: import_words.map(&:word), part: import_words.map(&:part)).pluck(:id)
+    #なぜか謎のloadが入ってしまうのでimportするのは一回だけ
+    values = ids.map{|id| "(" + ["NULL", id, tweet.id, "'#{Time.now.to_s(:db)}'", "'#{Time.now.to_s(:db)}'"].join(",") + ")" }
+    TwitterWordAppear.connection.execute("INSERT INTO `#{TwitterWordAppear.table_name}` (#{TwitterWordAppear.column_names.join(',')}) VALUES " + values.join(","))
+  rescue => e # => StandardError を catch
+    puts e.message
+  end
 end

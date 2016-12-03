289f11ed349191aca44d9e4b9772efa7413ec789
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-12-03 15:14:41 +0900

    多対多用のテーブルを作成

---
+++ server/app/models/twitter_word_appear.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: twitter_word_appears
+#
+#  id                   :integer          not null, primary key
+#  tweet_appear_word_id :integer          not null
+#  twitter_word_id      :integer          not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+# Indexes
+#
+#  twitter_word_appears_relation_index  (tweet_appear_word_id,twitter_word_id)
+#
+
+class TwitterWordAppear < ApplicationRecord
+end
---
+++ server/test/models/twitter_word_appear_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: twitter_word_appears
+#
+#  id                   :integer          not null, primary key
+#  tweet_appear_word_id :integer          not null
+#  twitter_word_id      :integer          not null
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#
+# Indexes
+#
+#  twitter_word_appears_relation_index  (tweet_appear_word_id,twitter_word_id)
+#
+
+require 'test_helper'
+
+class TwitterWordAppearTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

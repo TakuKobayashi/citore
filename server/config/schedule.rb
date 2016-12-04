4d6fc79f721d0ec5bd15cc4046dd2bbbb554121b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-12-05 01:47:02 +0900

    cronとして登録する素材を作った

---
+++ server/config/schedule.rb
@@ -0,0 +1,27 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+set :output, "#{path}/log/cron.log"
+
+every :day, at: "2:00 am" do
+  rake "batch:db_dump_and_upload"
+  command "/bin/echo `date`: Daily upload sql"
+end
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever

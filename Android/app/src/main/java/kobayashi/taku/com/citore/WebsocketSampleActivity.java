836bd0699bfeaddc5af3b7d534bcd2e424cadb2e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-12-26 10:38:29 +0900

    ActionCableでのWebsocketを試す

--- Android/app/src/main/java/kobayashi/taku/com/citore/WebsocketSampleActivity.java
+++ Android/app/src/main/java/kobayashi/taku/com/citore/WebsocketSampleActivity.java
@@ -21,7 +21,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.ResponseBody;
+import okhttp3.ws.WebSocket;
+import okhttp3.ws.WebSocketCall;
+import okhttp3.ws.WebSocketListener;
+import okio.Buffer;
 
 public class WebsocketSampleActivity extends Activity {
 
@@ -32,6 +39,39 @@ public class WebsocketSampleActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        Request request = new Request.Builder()
+                .url("ws://27.133.128.228/cable") // INPUT_YOUR_DOMEINは、heroku openしたときのドメインを指定してください。
+                .build();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+        WebSocketCall call = WebSocketCall.create(client, request);
+        call.enqueue(new WebSocketListener() {
+            @Override
+            public void onOpen(WebSocket webSocket, Response response) {
+                Log.d(Config.TAG, "onOpen");
+            }
+
+            @Override
+            public void onFailure(IOException e, Response response) {
+                Log.d(Config.TAG, "onFailure " + response, e);
+            }
+
+            @Override
+            public void onMessage(ResponseBody message) throws IOException {
+                Log.d(Config.TAG, "onMessage " + message.string());
+            }
+
+            @Override
+            public void onPong(Buffer payload) {
+                Log.d(Config.TAG, "onPong");
+            }
+
+            @Override
+            public void onClose(int code, String reason) {
+                Log.d(Config.TAG, "onClose");
+            }
+        });
         setContentView(R.layout.activity_main);
     }
 

e67c40072a19c9eab5dbe58433f93de79b2fa660
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-12-31 05:14:05 +0900

    pageの情報を保持

--- server/app/models/youtube_record.rb
+++ server/app/models/youtube_record.rb
@@ -7,10 +7,11 @@ class YoutubeRecord < ApplicationRecord
     apiconfig = YAML.load(File.open(Rails.root.to_s + "/config/apiconfig.yml"))
     youtube = Google::Apis::YoutubeV3::YouTubeService.new
     youtube.key = apiconfig["google_api"]["key"]
-    page_token = nil
+    page_token = ExtraInfo.read_extra_info[table_name]
     begin
       get_list = block.call(youtube, page_token)
       page_token = get_list.next_page_token
+      ExtraInfo.update({table_name => page_token})
     end while page_token.present?
   end
 end

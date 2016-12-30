a1b53cab1290bc742172b8fb4ec4e76703198007
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-12-31 05:01:34 +0900

    tagをつけました

--- server/app/models/youtube_video.rb
+++ server/app/models/youtube_video.rb
@@ -10,7 +10,6 @@
 #  description         :text(65535)
 #  thumnail_image_url  :string(255)      default(""), not null
 #  published_at        :datetime
-#  comment_count       :integer          default(0), not null
 #  dislike_count       :integer          default(0), not null
 #  like_count          :integer          default(0), not null
 #  favorite_count      :integer          default(0), not null
@@ -18,7 +17,6 @@
 #
 # Indexes
 #
-#  index_youtube_videos_on_comment_count       (comment_count)
 #  index_youtube_videos_on_published_at        (published_at)
 #  index_youtube_videos_on_video_id            (video_id) UNIQUE
 #  index_youtube_videos_on_youtube_channel_id  (youtube_channel_id)
@@ -27,9 +25,12 @@
 class YoutubeVideo < YoutubeRecord
   #belongs_to :channel, class_name: 'YoutubeChannel', foreign_key: :youtube_channel_id
   has_many :comments, class_name: 'YoutubeComment', foreign_key: :youtube_video_id
+  has_many :tags, class_name: 'YoutubeComment', foreign_key: :youtube_video_id
 
   def self.import_video!(youtube_video, category_id: nil, channel_id: nil)
-    videos = youtube_video.items.map do |item|
+    videos = []
+    id_and_tags = {}
+    youtube_video.items.each do |item|
       video = YoutubeVideo.new(
         youtube_category_id: category_id,
         youtube_channel_id: channel_id,
@@ -38,17 +39,24 @@ class YoutubeVideo < YoutubeRecord
         description: item.snippet.description,
         published_at: item.snippet.published_at,
         thumnail_image_url: item.snippet.thumbnails.default.url,
-        comment_count: item.statistics.comment_count,
         dislike_count: item.statistics.dislike_count,
         like_count: item.statistics.like_count,
         favorite_count: item.statistics.favorite_count,
         view_count: item.statistics.view_count
       )
-      video
+      id_and_tags[item.id] = item.snippet.tags
+      videos << video
     end
     updates = [:published_at]
     updates << :youtube_channel_id if channel_id.present?
     updates <<  :youtube_category_id if category_id.present?
     YoutubeVideo.import(videos, on_duplicate_key_update: updates)
+    video_ids = YoutubeVideo.where(video_id: id_and_tags.keys).pluck(:id, :video_id)
+    tags = video_ids.map do |id, video_id|
+      id_and_tags[video_id].map do |tag|
+        YoutubeVideoTag.new(youtube_video_id: id, tag: tag)
+      end
+    end.flatten
+    YoutubeVideoTag.import(tags)
   end
 end

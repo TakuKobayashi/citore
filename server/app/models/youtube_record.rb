fabc7fab8f031b348ba8d8af6cdcb0a13b380ae0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-03 03:36:32 +0900

    止まらないループ処理

--- server/app/models/youtube_record.rb
+++ server/app/models/youtube_record.rb
@@ -8,10 +8,22 @@ class YoutubeRecord < ApplicationRecord
     youtube = Google::Apis::YoutubeV3::YouTubeService.new
     youtube.key = apiconfig["google_api"]["key"]
     page_token = ExtraInfo.read_extra_info[table_name]
+    retry_counter = 0
     begin
-      get_list = block.call(youtube, page_token)
-      page_token = get_list.next_page_token
-      ExtraInfo.update({table_name => page_token})
+      begin
+        get_list = block.call(youtube, page_token)
+        page_token = get_list.next_page_token
+        ExtraInfo.update({table_name => page_token})
+        retry_counter = 0
+      rescue
+        retry_counter += 1
+        if retry_counter <= 5
+          retry
+        else
+          puts "retried 5 times so go to next loop"
+          break
+        end
+      end
     end while page_token.present?
   end
 end

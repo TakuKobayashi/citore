6a3ede4a8bb66e44fb467347fb10597fd3466766
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-09 05:28:13 +0900

    indexを修正

--- server/app/controllers/citore/words_controller.rb
+++ server/app/controllers/citore/words_controller.rb
@@ -1,6 +1,6 @@
 class Citore::WordsController < BaseController
   def index
-    readings = Citore::EroticWord.pluck(:reading)
+    readings = Citore::EroticWord.reading_words
     render :json => readings
   end
 

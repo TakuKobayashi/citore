02edfc01edaea4f85316d7e024757601a899a4ed
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-08 16:09:02 +0900

    nginxでリバースプロキシするので、socketファイルの生成位置をしっかり明記してあげないといけない

--- server/config/puma.rb
+++ server/config/puma.rb
@@ -14,6 +14,7 @@ port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
+bind "unix:/work/app/project/citore/server/tmp/sockets/puma.sock"
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together

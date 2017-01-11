bccd0861b75d2ab35ef65aa2df5a42f86275c4c3
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-12 02:35:35 +0900

    twitter_botでつぶやく

--- server/scripts/twitter_bot_tweet_stream.rb
+++ server/scripts/twitter_bot_tweet_stream.rb
@@ -25,14 +25,23 @@ sugarcoat_client = Twitter::REST::Client.new do |config|
   config.access_token_secret = extra_info["twitter_811024427487936512"]["token_secret"]
 end
 
+sugarcoat_keywords = ['@sugarcoat_bot', '#sugarcoat']
+citore_keywords = ['#citore', '@citore_bot']
+
 CacheStore.cache!
 stream_client = TweetStream::Client.new
-stream_client.track('#citore', '#Citore', '@citore_bot', '@sugarcoat_bot', '#sugarcoat', '#SugarCoat') do |status|
+stream_client.track('#citore', '@citore_bot', '@sugarcoat_bot', '#sugarcoat') do |status|
   next if status.lang != "ja" || ["811029389575979008", "811024427487936512"].include?(status.user.id.to_s)
 
   sanitaized_word = TwitterRecord.sanitized(status.text)
   without_url_tweet, urls = ApplicationRecord.separate_urls(sanitaized_word)
-  reading = ApplicationRecord.reading(without_url_tweet)
-  erotic_word = Citore::EroticWord.reading_words.detect{|r| reading.include?(r) }
-  citore_client.update("@#{status.user.screen_name} テスト")
+  if citore_keywords.any?{|k| without_url_tweet.include?(k) }
+    reading = ApplicationRecord.reading(without_url_tweet)
+    erotic_word = Citore::EroticWord.reading_words.detect{|r| reading.include?(r) }
+    citore_client.update("@#{status.user.screen_name} テスト")
+  end
+  if citore_keywords.any?{|k| without_url_tweet.include?(k) }
+    reading = ApplicationRecord.reading(without_url_tweet)
+    sugarcoat_client.update("@#{status.user.screen_name} テスト")
+  end
 end

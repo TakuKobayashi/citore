f3bce7e7f48c35d27195440e1bbd614bc235cc36
TakuKobayashi <takobayashi@ncjapan.co.jp>
2017-01-11 16:44:46 +0900

    appear_wordをdynamoに持っていくための準備

---
+++ server/db/migrate/20170111053913_create_appear_word_dynamos.rb
@@ -0,0 +1,18 @@
+class CreateAppearWordDynamos < ActiveRecord::Migration[5.0]
+  def change
+    return if AppearWordDynamo.table_exists?
+    migration = Aws::Record::TableMigration.new(AppearWordDynamo)
+    migration.create!(
+      provisioned_throughput: {
+        read_capacity_units: 5,
+        write_capacity_units: 5
+      }
+    )
+    migration.wait_until_available
+  end
+
+  def down
+    migration = Aws::Record::TableMigration.new(AppearWordDynamo)
+    migration.delete!
+  end
+end
---
+++ server/test/models/appear_word_dynamo_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AppearWordDynamoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

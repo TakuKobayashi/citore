a798dae9cf22ec82a147609aed0a66f46322ec59
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-11 23:56:06 +0900

    マルコフ連鎖のテーブル構造変更

--- server/db/migrate/20170111083834_create_markov_trigrams.rb
+++ server/db/migrate/20170111083834_create_markov_trigrams.rb
@@ -2,13 +2,12 @@ class CreateMarkovTrigrams < ActiveRecord::Migration[5.0]
   def change
     create_table :markov_trigrams do |t|
       t.string :source_type, null: false
-      t.integer :source_id, null: false
       t.string :first_gram, null: false, default: ""
       t.string :second_gram, null: false, default: ""
       t.string :third_gram, null: false, default: ""
-      t.boolean :is_end, null: false, default: false
+      t.integer :appear_count, null: false, default: 0
     end
-    add_index :markov_trigrams, [:source_type, :source_id]
+    add_index :markov_trigrams, [:source_type, :first_gram, :second_gram, :third_gram], name: "markov_trigram_type_word_index"
     add_index :markov_trigrams, [:first_gram, :second_gram, :third_gram], name: "markov_trigram_word_index"
   end
 end

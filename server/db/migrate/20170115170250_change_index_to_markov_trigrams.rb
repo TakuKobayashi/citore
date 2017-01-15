ed8f2759f1a3b06563e5464f9209531f3fd0065b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-16 02:13:17 +0900

    こうやんのかな?

--- server/db/migrate/20170115170250_change_index_to_markov_trigrams.rb
+++ server/db/migrate/20170115170250_change_index_to_markov_trigrams.rb
@@ -1,6 +1,6 @@
 class ChangeIndexToMarkovTrigrams < ActiveRecord::Migration[5.0]
   def change
-    remove_index :markov_trigrams, "markov_trigram_word_index"
+    remove_index :markov_trigrams, [:first_gram, :second_gram, :third_gram]
     add_index :markov_trigrams, :first_gram
   end
 end

5f1cd93470fee9c26613033d6f0ae13c1a465b8b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-16 04:45:18 +0900

    一瞬だけ戻す

--- server/db/migrate/20170115192244_add_index_to_twitter_words.rb
+++ server/db/migrate/20170115192244_add_index_to_twitter_words.rb
@@ -1,6 +1,5 @@
 class AddIndexToTwitterWords < ActiveRecord::Migration[5.0]
   def change
-    remove_index :twitter_words, :twitter_tweet_id
-    add_index :twitter_words, :twitter_tweet_id, unique: true
+    add_index :twitter_words, :twitter_tweet_id
   end
 end

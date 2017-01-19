facc39eaacd521efbc17b1c32de4293bd390abc8
TakuKobayashi <takobayashi@ncjapan.co.jp>
2017-01-19 18:14:16 +0900

    継承させた

---
+++ server/app/models/expression_categorised_word.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: categorised_words
+#
+#  id              :integer          not null, primary key
+#  type            :string(255)      not null
+#  large_category  :integer          not null
+#  medium_category :string(255)      not null
+#  detail_category :string(255)      not null
+#  degree          :integer          default("unknown"), not null
+#  body            :string(255)      not null
+#  from_url        :string(255)
+#
+# Indexes
+#
+#  index_categorised_words_on_from_url  (from_url)
+#  word_categories_index                (large_category,medium_category,detail_category)
+#
+
+class ExpressionCategorisedWord < CategorisedWord
+  JAPANESE_HYOGEN_URL = "http://hyogen.info/"
+end

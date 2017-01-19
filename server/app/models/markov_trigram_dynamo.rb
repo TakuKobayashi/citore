391403751e5173ebecfbb61ad9b32054fd98f852
TakuKobayashi <takobayashi@ncjapan.co.jp>
2017-01-19 12:00:06 +0900

    malkovtrigramのテーブル変更

--- server/app/models/markov_trigram_dynamo.rb
+++ server/app/models/markov_trigram_dynamo.rb
@@ -1,10 +1,9 @@
 class MarkovTrigramDynamo
   include Aws::Record
 
-  string_attr :first_gram, hash_key: true
-  integer_attr :id, range_key: true
+  string_attr :prefix, hash_key: true
   string_attr :source_type, range_key: true
-  string_attr :second_gram
-  string_attr :third_gram
-  integer_attr :appear_count
+  integer_attr :id
+  integer_attr :state
+  array_attr :others_json
 end

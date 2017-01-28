5a5ef62592da6f769d67083769b2de1fd14396a5
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-28 18:55:58 +0900

    hookの部分を変更

--- server/app/controllers/moi_voice/streaming_controller.rb
+++ server/app/controllers/moi_voice/streaming_controller.rb
@@ -3,16 +3,28 @@ class MoiVoice::StreamingController < BaseController
   layout "moi_voice"
 
   def play
-    twitcast_user = MoiVoice::TwitcasUser.find_by(id: params[:user_id])
-    logger.info twitcast_user.try(:attributes)
-    live_straem = twitcast_user.live_straems.find_by(state: [MoiVoice::LiveStream.states[:stay], MoiVoice::LiveStream.states[:playing]])
-    if live_straem.blank?
-      twitcast_user.live_straems.create(state: MoiVoice::LiveStream.states[:stay])
-    end
+    @twitcast_user = MoiVoice::TwitcasUser.find_by(id: params[:user_id])
+    load_or_create
   end
 
   def hook
     payload = JSON.parse(param[:payload])
+    @twitcast_user = MoiVoice::TwitcasUser.find_by(twitcast_uesr_screen_id: payload["userid"])
+    live_straem = load_or_create
+    if payload["action"] == "live_start"
+      live_straem.update!(state: MoiVoice::LiveStream.states[:playing], started_at: Time.now)
+    elsif payload["action"] == "live_end"
+      live_straem.update!(state: MoiVoice::LiveStream.states[:finish], started_at: Time.now)
+    end
     head(:ok)
+  end
+
+  private
+  def load_or_create
+    live_straem = @twitcast_user.live_straems.find_by(state: [MoiVoice::LiveStream.states[:stay], MoiVoice::LiveStream.states[:playing]])
+    if live_straem.blank?
+      live_straem = twitcast_user.live_straems.create(state: MoiVoice::LiveStream.states[:stay])
+    end
+    return live_straem
   end
 end

4aa43276365ebbb11bf3be8c404a218125afd89b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-28 15:53:24 +0900

    ツイキャスログイン、そしてユーザー情報を作成

--- server/app/models/moi_voice/twitcas_user.rb
+++ server/app/models/moi_voice/twitcas_user.rb
@@ -17,4 +17,31 @@
 #
 
 class MoiVoice::TwitcasUser < ApplicationRecord
+  TWITCAS_API_URL_ROOT = "https://apiv2.twitcasting.tv"
+
+  def self.get_oauth_url
+    apiconfig = YAML.load(File.open(Rails.root.to_s + "/config/apiconfig.yml"))
+    return TWITCAS_API_URL_ROOT + "/oauth2/authorize?client_id=" + apiconfig["twitcas"]["client_id"] + "&response_type=code"
+  end
+
+  def self.oauth!(code, redirect_url)
+    apiconfig = YAML.load(File.open(Rails.root.to_s + "/config/apiconfig.yml"))
+    http_client = HTTPClient.new
+    request_params = apiconfig["twitcas"].merge({
+      code: code,
+      grant_type: "authorization_code",
+      redirect_uri: redirect_url
+    })
+    response_oauth = http_client.post(TWITCAS_API_URL_ROOT + "/oauth2/access_token", request_params, {'Content-Type' => 'application/json;charset=UTF-8'})
+    hash = JSON.parse(response_oauth.body)
+    request_user_header = {
+      'Content-Type' => 'application/json;charset=UTF-8',
+      'Authorization' => 'Bearer ' + hash["access_token"]
+    }
+    response_user = http_client.get(TWITCAS_API_URL_ROOT + "/verify_credentials", {}, request_user_header)
+    user_hash = JSON.parse(response_user.body)
+    user = MoiVoice::TwitcasUser.find_or_initialize_by(twitcas_user_id: user_hash["user"]["id"])
+    user.update!(access_token: user_hash["access_token"], name: user_hash["user"]["name"], last_logined_at: Time.now, expires_in: hash["expires_in"])
+    return user
+  end
 end

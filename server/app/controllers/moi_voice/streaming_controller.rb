ae77992ddee68351da2494e67ca3deb98924a2ed
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-01-28 16:26:05 +0900

    stateを取得

--- server/app/controllers/moi_voice/streaming_controller.rb
+++ server/app/controllers/moi_voice/streaming_controller.rb
@@ -1,4 +1,7 @@
 class MoiVoice::StreamingController < BaseController
   def play
+    twitcast_user = MoiVoice::TwitcasUser.find_by(id: params[:user_id])
+    logger.info twitcast_user.try(:attributes)
+    live_straem = twitcast_user.live_straems.find_or_create_by(state: [MoiVoice::LiveStream.states[:stay], MoiVoice::LiveStream.states[:playing]])
   end
 end
--- server/db/migrate/20170128051608_create_moi_voice_live_streams.rb
+++ server/db/migrate/20170128051608_create_moi_voice_live_streams.rb
@@ -2,10 +2,12 @@ class CreateMoiVoiceLiveStreams < ActiveRecord::Migration[5.0]
   def change
     create_table :moi_voice_live_streams do |t|
       t.integer :moi_voice_twitcas_user_id, null: false
-      t.datetime :start_at, null: false
+      t.datetime :started_at
+      t.datetime :finished_at
       t.integer :state, null: false
       t.timestamps
     end
-    add_index :moi_voice_live_streams, :moi_voice_twitcas_user_id, name: "moi_voice_live_streams_user_id_index"
+    add_index :moi_voice_live_streams, [:moi_voice_twitcas_user_id, :state], name: "moi_voice_live_streams_user_id_index"
+    add_index :moi_voice_live_streams, [:started_at, :finished_at], name: "moi_voice_live_finished_at_index"
   end
 end

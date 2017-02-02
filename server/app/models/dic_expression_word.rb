58016b8f30d0299128bd035b97b315b4e9725a50
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-02-02 21:51:53 +0900

    カテゴリを保存できるようにしておく一応

--- server/app/models/dic_expression_word.rb
+++ server/app/models/dic_expression_word.rb
@@ -8,12 +8,11 @@
 #  medium_category :integer          default("medium_unknown"), not null
 #  detail_category :string(255)      not null
 #  body            :text(65535)      not null
-#  from_url        :string(255)
+#  relation_id_csv :text(65535)
 #
 # Indexes
 #
-#  index_categorised_words_on_from_url  (from_url)
-#  word_categories_index                (large_category,medium_category,detail_category)
+#  word_categories_index  (large_category,medium_category,detail_category)
 #
 
 class DicExpressionWord < CategorisedWord
--- server/db/migrate/20170118165739_create_categorised_words.rb
+++ server/db/migrate/20170118165739_create_categorised_words.rb
@@ -6,9 +6,8 @@ class CreateCategorisedWords < ActiveRecord::Migration[5.0]
       t.integer :medium_category, null: false, default: 0
       t.string :detail_category, null: false
       t.text :body, null: false
-      t.string :from_url
+      t.text :relation_id_csv
     end
     add_index :categorised_words, [:large_category, :medium_category, :detail_category], name: "word_categories_index"
-    add_index :categorised_words, :from_url
   end
 end

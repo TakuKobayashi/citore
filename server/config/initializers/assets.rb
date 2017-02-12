090dfcbfa2dd8f44df2a36a7b61b6ea79fe28fa4
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-02-12 18:34:16 +0900

    three.jsを追加読み込み

--- server/config/initializers/assets.rb
+++ server/config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( moi_voice/style.css )
+Rails.application.config.assets.precompile += %w( three.min.js )
+

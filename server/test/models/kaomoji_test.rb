719cd7658f13c031e358a343a49021e5b8d6195e
TakuKobayashi <takobayashi@ncjapan.co.jp>
2017-02-20 10:14:45 +0900

    kaomojiテーブルやめて、category_wordにする

--- server/test/models/kaomoji_test.rb
+++ server/test/models/kaomoji_test.rb
@@ -1,16 +1,18 @@
 # == Schema Information
 #
-# Table name: kaomojis
+# Table name: categorised_words
 #
-#  id       :integer          not null, primary key
-#  category :string(255)      not null
-#  meaning  :string(255)      not null
-#  body     :string(255)      not null
+#  id              :integer          not null, primary key
+#  type            :string(255)      not null
+#  large_category  :integer          default("large_unknown"), not null
+#  medium_category :integer          default("medium_unknown"), not null
+#  detail_category :string(255)      not null
+#  body            :text(65535)      not null
+#  description     :text(65535)
 #
 # Indexes
 #
-#  index_kaomojis_on_category  (category)
-#  index_kaomojis_on_meaning   (meaning)
+#  word_categories_index  (large_category,medium_category,detail_category)
 #
 
 require 'test_helper'

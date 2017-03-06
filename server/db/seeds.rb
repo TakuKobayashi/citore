21a2fae398b9fa5c75f98d1dfe8dba0365c1970c
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-03-06 11:48:22 +0900

    seedを整理した。ネガポジはrakeタスクで入れればいいのでは?

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -6,44 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-dics = []
-en = File.read(Rails.root.to_s + "/scripts/dictionary/pn_en.dic")
-ens = en.split("\n")
-ens.each do |e|
-  es = e.split(":")
-  dic = EmotionalWordDynamo.find(word: es[0], reading: es[0], part: es[1])
-  next if dic.present?
-  dic = EmotionalWordDynamo.new
-  dic.word = es[0]
-  dic.reading = es[0]
-  dic.part = es[1]
-  dic.score = es[2].to_f
-  dic.save!
-  p "w:#{es[0]} r:#{es[0]} p:#{es[1]} s:#{es[2]}"
-end
-
-parts = EmotionalWordDynamo::PARTS
-ja = File.read(Rails.root.to_s + "/scripts/dictionary/pn_ja.dic")
-jas = ja.split("\r\n")
-jas.each do |j|
-  js = j.split(":")
-  word = ApplicationRecord.sanitized(js[0])
-  reading = ApplicationRecord.reading(js[1])
-  dic = EmotionalWordDynamo.find(word: word, reading: reading,part:  parts[js[2]])
-  next if dic.present?
-  dic = EmotionalWordDynamo.new
-  dic.word = word
-  dic.reading = reading
-  dic.part = parts[js[2]]
-  dic.score = js[3].to_f
-  p "w:#{word} r:#{reading} p:#{js[2]} s:#{js[3]}"
-  dic.save!
-end
-
-ens_average_score = ens.inject(0){|result, cell| result += cell.split(":")[2].to_f} / ens.size
-jas_average_score = jas.inject(0){|result, cell| result += cell.split(":")[3].to_f} / jas.size
-
-hash = {}
-hash[:en_average_score] = ens_average_score
-hash[:ja_average_score] = jas_average_score
-ExtraInfo.update(hash)
+AdminUser.find_or_create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')

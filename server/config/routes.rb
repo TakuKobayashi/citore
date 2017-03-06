1d704b3190aa6f85a937ac0561f2e688f5f5655b
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-03-06 11:03:48 +0900

    Revert "rails_adminをいれてみた"
    This reverts commit 92bf9037c1dcbc6e33b065b5afecb5da52d4dabf.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/auth/:provider/callback" => "sns#oauth_callback"

ebb260b8bd88b4322982729679636810b4e08e46
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-03-30 14:01:20 +0900

    importができていないままでしたので修正

--- server/app/admin/data_batches.rb
+++ server/app/admin/data_batches.rb
@@ -3,16 +3,17 @@ ActiveAdmin.register_page "DataBatches" do
 
   content title: "データベース操作" do
     panel "エクセルまたはCSVのデータをインポート" do
-      active_admin_form_for(:import, url: active_admin_masters_upload_path) do |f|
+      active_admin_form_for(:import, url: admin_databatches_import_data_path) do |f|
         f.inputs do
           f.input :file, as: :file, label: "ファイルを選択"
+          f.input :before_truncate, as: :check_boxes, collection: ["Yes"], label: "インポートする前にデータベースを空にする?"
           f.submit
         end
       end
     end
 
     panel "DBのデータをS3にアップロード" do
-      active_admin_form_for(:upload_to_s3, url: active_admin_masters_upload_path) do |f|
+      active_admin_form_for(:upload_to_s3, url: admin_databatches_upload_to_db_dump_to_s3_path) do |f|
         f.inputs do
           f.submit
         end
@@ -32,6 +33,11 @@ ActiveAdmin.register_page "DataBatches" do
       gsub(/spot_gacha_/, 'spot_gacha/').
       gsub(/sugarcoat_/, 'sugarcoat/').
       camelcase).constantize
+
+    if params[:import][:before_truncate].include?("Yes")
+      clazz.connection.execute("TRUNCATE TABLE #{clazz.table_name}")
+    end
+
     data = CSV.read(file.tempfile.path, headers: true)
     arr = data.map{|d| clazz.new(data) }
     ActiveRecord::Base.transaction do

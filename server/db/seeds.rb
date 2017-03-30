649bed6e1fac078e6a57495fd30c0d8b8221bcca
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-03-30 23:01:53 +0900

    active_admin関係の変なところを直した

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AdminUser.find_or_create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')

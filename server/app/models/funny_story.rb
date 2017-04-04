450448da001ca47054acd8119e251bf5261f027d
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-04-04 11:39:23 +0900

    面白いお話も集めてみようかなと

---
+++ server/app/models/funny_story.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: categorised_words
+#
+#  id              :integer          not null, primary key
+#  type            :string(255)      not null
+#  large_category  :integer          default("large_unknown"), not null
+#  medium_category :string(255)      default(""), not null
+#  detail_category :string(255)      not null
+#  body            :text(65535)      not null
+#  description     :text(65535)
+#
+# Indexes
+#
+#  word_categories_index  (large_category,medium_category,detail_category)
+#
+
+class FunnyStory < CategorisedWord
+end

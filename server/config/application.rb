1c551b57fc43046991802b1c93773c4394caf550
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-06-21 22:21:33 +0900

    railsのversionを5.1煮上げてみた

--- server/config/application.rb
+++ server/config/application.rb
@@ -11,6 +11,8 @@ module Server
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.load_defaults 5.1
+
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :local
 

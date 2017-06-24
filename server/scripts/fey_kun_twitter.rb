838104b27cc5d1bf9e3941640dcc2ccad8edf1fe
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-06-24 13:41:14 +0900

    fay_kunのおうむ返しの機能を入れた

---
+++ server/scripts/fey_kun_twitter.rb
@@ -0,0 +1,29 @@
+apiconfig = YAML.load(File.open(Rails.root.to_s + "/config/apiconfig.yml"))
+TweetStream.configure do |config|
+  config.consumer_key       = apiconfig["twitter"]["fey_kun_ai"]["consumer_key"]
+  config.consumer_secret    = apiconfig["twitter"]["fey_kun_ai"]["consumer_secret"]
+  config.oauth_token        = apiconfig["twitter"]["fey_kun_ai"]["bot"]["access_token_key"]
+  config.oauth_token_secret = apiconfig["twitter"]["fey_kun_ai"]["bot"]["access_token_secret"]
+  config.auth_method        = :oauth
+end
+
+natto = ApplicationRecord.get_natto
+
+extra_info = ExtraInfo.read_extra_info
+
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+rest_client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = apiconfig["twitter"]["fey_kun_ai"]["consumer_key"]
+  config.consumer_secret     = apiconfig["twitter"]["fey_kun_ai"]["consumer_secret"]
+  config.access_token        = apiconfig["twitter"]["fey_kun_ai"]["bot"]["access_token_key"]
+  config.access_token_secret = apiconfig["twitter"]["fey_kun_ai"]["bot"]["access_token_secret"]
+end
+
+stream_client = TweetStream::Client.new
+stream_client.userstream do |status|
+  if status.in_reply_to_screen_name == "fey_kun_ai" && status.user.screen_name != "fey_kun_ai"
+    sanitized_text = TwitterRecord.sanitized(status.text)
+    rest_client.update("@#{status.user.screen_name}\n#{sanitized_text}", options = {in_reply_to_status_id: status.id})
+  end
+end

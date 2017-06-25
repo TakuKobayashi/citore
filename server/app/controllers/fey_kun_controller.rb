6d96d6d767abe50be4cb8457d58871a3d4d4243c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-06-25 11:28:07 +0900

    送られてくるデータの形をかました

--- server/app/controllers/fey_kun_controller.rb
+++ server/app/controllers/fey_kun_controller.rb
@@ -5,16 +5,14 @@ class FeyKunController < BaseController
   end
 
   def analized
-    result = JSON.parse(request.body.read, {:symbolize_names => true})
-
-    logger.info result
     logger.info params
-
-    image = FeyKunAi::InquiryTweetImage.find_by(id: result["image_id"])
+    image = FeyKunAi::InquiryTweetImage.find_or_initialize_by(id: params[:image_id])
     image.output ||= {}
+
     object_image_name = FeyKunAi::InquiryTweetImage.upload_s3(params[:object_img])
     err_image_name = FeyKunAi::InquiryTweetImage.upload_s3(params[:err_img])
-    image.output = image.output.merge(result["result"].merge(object_image_name: object_image_name, err_image_name: err_image_name))
+
+    image.output = image.output.merge(params[:result].merge(object_image_name: object_image_name, err_image_name: err_image_name))
     image.save!
     head(:ok)
   end

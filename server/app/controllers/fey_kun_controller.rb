3df8557363a382e6fb90022647004b152eea2256
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-06-25 11:31:45 +0900

    パラメータ修正とチェック

--- server/app/controllers/fey_kun_controller.rb
+++ server/app/controllers/fey_kun_controller.rb
@@ -5,12 +5,15 @@ class FeyKunController < BaseController
   end
 
   def analized
+  	body = request.body.read
+  	logger.info body
+
     logger.info params
     image = FeyKunAi::InquiryTweetImage.find_or_initialize_by(id: params[:image_id])
     image.output ||= {}
 
     object_image_name = FeyKunAi::InquiryTweetImage.upload_s3(params[:object_img])
-    err_image_name = FeyKunAi::InquiryTweetImage.upload_s3(params[:err_img])
+    err_image_name = FeyKunAi::InquiryTweetImage.upload_s3(params[:error_img])
 
     image.output = image.output.merge(params[:result].merge(object_image_name: object_image_name, err_image_name: err_image_name))
     image.save!

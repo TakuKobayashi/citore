c085ecd5db86fb1f624094989cfc0564af2ffe61
TakuKobayshi <>
2017-07-03 17:58:37 +0900

    ブラウザに合わせたサイズになるようにした

--- server/app/assets/javascripts/homepage/top.js
+++ server/app/assets/javascripts/homepage/top.js
@@ -5,12 +5,8 @@ var imageLoad = function(url, onload){
   var img = new Image();
   img.src = url;
   img.onload = function() {
-  	onload(img);
+    onload(img);
   }
-}
-
-var drawImage = function(canvas, image){
-  canvas.drawImage(image, 0, 0);
 }
 
 $(document).ready(function(){
@@ -18,32 +14,34 @@ $(document).ready(function(){
   var ctx = canvas.getContext('2d');
   var imagepathes = $('.top-mv-visual-bg-item').data('imagepathes');
 
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+
   var resizeCanvas = function(){
-  	console.log(images.length);
-    var width = window.innerWidth;
-    var height = window.innerHeight;
+    width = window.innerWidth;
+    height = window.innerHeight;
     $('#bgimage').attr('width', width);
     $('#bgimage').attr('height', height);
     if(images.length > 0){
-      drawImage(ctx, images[bgRenderIndex]);
+      ctx.drawImage(images[bgRenderIndex], 0, 0, width, height);
     }
   }
 
   resizeCanvas();
 
   for(var i = 0;i < imagepathes.length;++i){
-  	imageLoad(imagepathes[i], function(image){
-  	  if(images.length == 0){
-  	    drawImage(ctx, image);
-  	  }
+    imageLoad(imagepathes[i], function(image){
+      if(images.length == 0){
+        ctx.drawImage(image, 0, 0, width, height);
+      }
       images.push(image);
-  	});
+    });
   }
   window.addEventListener('resize', resizeCanvas, false);
 
   setInterval(function(){
-  	bgRenderIndex++;
-  	bgRenderIndex = bgRenderIndex % images.length
-    drawImage(ctx, images[bgRenderIndex]);
+    bgRenderIndex++;
+    bgRenderIndex = bgRenderIndex % images.length;
+    ctx.drawImage(images[bgRenderIndex], 0, 0, width, height);
   }, 3000);
 });

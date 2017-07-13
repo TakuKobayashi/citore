c04a880a6d0cbffb0b1ebaa4d082abe7f3a5f23a
TakuKobayshi <>
2017-07-13 15:56:55 +0900

    新しく生まれ変わらせるために設定とかレイアウトとか変更しました

--- server/config/initializers/assets.rb
+++ server/config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( *.js *application.css)
-Rails.application.config.assets.precompile += %w( feykun*.css homepage*css)
+Rails.application.config.assets.precompile += %w( feykun*.css homepage*.css libraries*.css)

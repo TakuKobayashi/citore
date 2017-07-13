9e4a79f87e804847dc907b5ed63ff0d466b6950f
TakuKobayshi <>
2017-07-13 17:41:35 +0900

    ホームページ作成における雑多ものを追加

--- server/app/controllers/products_controller.rb
+++ server/app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < BaseController
   def index
+    @products = Homepage::Product.where("pubulish_at > ?", Time.current).order("pubulish_at DESC").page(params[:page]).per(10)
   end
 end

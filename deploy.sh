0ca442e28d5b8eb0a10e649f4c36411872549320
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-07-18 01:50:36 +0900

    citoreはnode.jsもあるのでそれも考慮する

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 git pull
+cd ./nodejs/
+npm update
+forever restart index.js
+cd ../server/
 bundle install
 RAILS_ENV=production rails db:migrate
 RAILS_ENV=production rails assets:clean
@@ -8,3 +12,4 @@ RAILS_ENV=production rails assets:precompile
 whenever --update-crontab
 kill -9 `cat tmp/pids/server.pid`
 SECRET_KEY_BASE=$(rake secret) rails server -e production -p 3100 -d
+cd ../

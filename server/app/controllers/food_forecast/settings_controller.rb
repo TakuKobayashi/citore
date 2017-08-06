4e1041c27674bb6b09b554df51078bc24d33b4a2
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-06 11:28:18 +0900

    データを無理くり入れるように

--- server/app/controllers/food_forecast/settings_controller.rb
+++ server/app/controllers/food_forecast/settings_controller.rb
@@ -6,10 +6,24 @@ class FoodForecast::SettingsController < FoodForecast::BaseController
   end
 
   def register
-    if params[:token].blank?
+    if params[:token].present?
+      user = FoodForecast::User.find_by!(token: params[:token])
+    else
       user = FoodForecast::User.create!
-      cookies[:token] = user.token
     end
+    period =  FoodForecast::UserPeriod.find_or_initialize_by(user_id: user.id)
+    if params[:rhythm_first].present?
+      period.first_at = Time.parse(params[:rhythm_first])
+    else
+      period.first_at = Time.current
+    end
+    period.first_span_day = params[:rhythm_a].to_i * 100 + params[:rhythm_b].to_i * 10 + params[:rhythm_c].to_i
+    period.second_at = period.first_at
+    period.third_at = period.second_at
+    period.second_span_day = period.first_span_day
+    period.save!
+
+    cookies[:token] = user.token
     redirect_to food_forecast_root_url
-  end 
+  end
 end

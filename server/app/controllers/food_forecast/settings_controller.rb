bd9345b8ca48ad4a8cc287bf1ee83ba9b9f8a81e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-06 10:53:04 +0900

    画面とかユーザー作ったりだとか...

--- server/app/controllers/food_forecast/settings_controller.rb
+++ server/app/controllers/food_forecast/settings_controller.rb
@@ -1,8 +1,15 @@
 class FoodForecast::SettingsController < FoodForecast::BaseController
   def input
+    if cookies[:token].present?
+      @current_user = FoodForecast::User.find_by(token: cookies[:token])
+    end
   end
 
   def register
+    if params[:token].blank?
+      user = FoodForecast::User.create!
+      cookies[:token] = user.token
+    end
     redirect_to food_forecast_root_url
-  end
+  end 
 end

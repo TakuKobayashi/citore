e0f8fd3b24627c99cbf8cf2c5f20a9661be5bfbc
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-11 15:45:56 +0900

    qiitaとslideshareついか

---
+++ server/app/models/homepage/qiita.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: homepage_articles
+#
+#  id            :integer          not null, primary key
+#  type          :string(255)
+#  title         :string(255)      not null
+#  description   :text(65535)
+#  url           :string(255)      not null
+#  thumbnail_url :string(255)
+#  pubulish_at   :datetime         not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_homepage_articles_on_pubulish_at  (pubulish_at)
+#
+
+class Homepage::Qiita < Homepage::Article
+end

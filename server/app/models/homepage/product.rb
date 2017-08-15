3eeef2a98b44dbbe5401f901d8377bca10a7d0df
TakuKobayshi <>
2017-08-15 13:55:49 +0900

    urlをいれればOGPから色々と勝手に設定してくれるようにした

--- server/app/models/homepage/product.rb
+++ server/app/models/homepage/product.rb
@@ -19,6 +19,50 @@
 #
 
 class Homepage::Product < ApplicationRecord
+  enum category: [
+    :others,
+    :application,
+    :game,
+    :web,
+    :iot,
+    :vrarmr,
+    :bot
+  ]
+
+  before_create do
+    if url.present?
+      og = OpenGraph.new(self.url.to_s)
+      if self.title.blank?
+        self.title = og.title
+      end
+      if self.description.blank?
+        self.description = og.description
+      end
+      if self.thumbnail_url.blank? && self.large_image_url.blank?
+        self_url = Addressable::URI.parse(self.url)
+        image_urls = og.images.map do |image_url|
+          img_url = Addressable::URI.parse(image_url)
+          if img_url.scheme.blank?
+            img_url.scheme = self_url.scheme
+          end
+          if img_url.host.blank?
+            img_url.host = self_url.host
+          end
+          img_url.to_s
+        end
+        thumbnail_image_url, l_image_url = image_urls.minmax_by do |url|
+          size = FastImage.size(url)
+          size[0] * size[1]
+        end
+        self.thumbnail_url = thumbnail_image_url
+        self.large_image_url = l_image_url
+      end
+    end
+    if self.pubulish_at.blank?
+      self.pubulish_at = Time.current
+    end
+  end
+
   after_create do
     announcement = Homepage::Announcement.find_or_initialize_by(from: self)
     announcement.update!(

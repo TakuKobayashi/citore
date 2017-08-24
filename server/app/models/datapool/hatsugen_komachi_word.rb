b9cd0241880c14d8edb8441474df0d4251d82890
TakuKobayshi <>
2017-08-24 19:12:56 +0900

    Datapool継承

---
+++ server/app/models/datapool/hatsugen_komachi_word.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: datapool_appear_words
+#
+#  id             :integer          not null, primary key
+#  appear_count   :integer          default(0), not null
+#  type           :string(255)
+#  word           :string(255)      not null
+#  part           :string(255)      not null
+#  reading        :string(255)      not null
+#  sentence_count :integer          default(0), not null
+#
+# Indexes
+#
+#  index_datapool_appear_words_on_reading        (reading)
+#  index_datapool_appear_words_on_word_and_part  (word,part) UNIQUE
+#
+
+class Datapool::HatsugenKomachiWord < Datapool::AppearWord
+end 

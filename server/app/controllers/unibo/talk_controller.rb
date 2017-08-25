8d67ad5d2887ab2b939bfd575c1f5f8acce6be3e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-25 14:05:34 +0900

    定型文には返せるようにする

--- server/app/controllers/unibo/talk_controller.rb
+++ server/app/controllers/unibo/talk_controller.rb
@@ -9,7 +9,31 @@ class Unibo::TalkController < BaseController
 
   def say
     word = params[:word]
-    data = Datapool::HatsugenKomachi.find_by(id: rand(Datapool::HatsugenKomachi.count) + 1)
-    render :json => data.try(:title)
+    res_word = dummy_res
+    if res_word.blank?
+      data = Datapool::HatsugenKomachi.find_by(id: rand(Datapool::HatsugenKomachi.count) + 1)
+      res_word = data.try(:title)
+    end
+    render :json => res_word
+  end
+
+  private
+  def dummy_res
+    word = params[:word].to_s
+    reading = ApplicationRecord.reading(word)
+    if reading.start_with?("ハァ")
+      return "どうしたの？"
+    elsif reading.start_with?("ナンカサイキンヤルキガオキナインダヨネイエニイルトキハダイタイネットサーフィンシテルカソシャゲシテルダケダシ")
+      return "家にいるとついダラダラとネットサーフィンしちゃうよね。やらなきゃいけないことがある時ほどやる気が起こらなかったり。"
+    elsif reading.start_with?("ソウナンダヨヤラナイトイケナイコトハタクサンアッテナノニダラダラシチャウヤラナキャイケナイコトヲアトマワシニシテルトジブンガホントウニダメナニンゲンニオモエテクル")
+      return "なんでだろうね？"
+    elsif reading.start_with?("ワカラナイ")
+      return "そうだよね…"
+    elsif reading.start_with?("デモヤラナキャイケナイコトガアルトキホドヤルキガオコラナイッテノハホントウニソウナンダヨナァコンカイモレポートノテイシュツガアトニシュウカンダッテオモッタラナンカキュウニナニモヤリタクナクナッテキチャッテ")
+      return "どうしてそう思う？"
+    elsif reading.start_with?("ドウシテッテ…ヤラナキャイケナイコトカラニゲテアソンジャッテルカラソリャジブンハダメニンゲンダッテオモッチャウヨ")
+      return "そっかぁ"
+    end
+    return nil
   end
 end

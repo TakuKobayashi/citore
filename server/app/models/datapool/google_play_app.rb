35e3ba23fa536f6a2c95ab68cb4a47de4d0834a0
TakuKobayshi <>
2017-09-15 14:56:15 +0900

    google playのURLも登録

--- server/app/models/datapool/google_play_app.rb
+++ server/app/models/datapool/google_play_app.rb
@@ -23,6 +23,19 @@
 #
 
 class Datapool::GooglePlayApp < Datapool::StoreProduct
+  URLS_HASH = {
+    top_grossing: "https://play.google.com/store/apps/collection/topgrossing?hl=ja",
+    top_grossing_game: "https://play.google.com/store/apps/category/GAME/collection/topgrossing?hl=ja",
+    top_free: "https://play.google.com/store/apps/collection/topselling_free?hl=ja",
+    top_free_game: "https://play.google.com/store/apps/category/GAME/collection/topselling_free?hl=ja",
+    top_paid: "https://play.google.com/store/apps/collection/topselling_paid?hl=ja",
+    top_paid_game: "https://play.google.com/store/apps/category/GAME/collection/topselling_paid?hl=ja",
+    new_games: "https://play.google.com/store/apps/category/GAME/collection/topselling_new_free?hl=ja",
+    new_games_paid: "https://play.google.com/store/apps/category/GAME/collection/topselling_new_paid?hl=ja",
+    new_apps: "https://play.google.com/store/apps/collection/topselling_new_free?hl=ja",
+    new_apps_paid: "https://play.google.com/store/apps/collection/topselling_new_paid?hl=ja"
+  }
+
   def self.update_rankings!
 
   end
--- server/app/models/datapool/itunes_store_app.rb
+++ server/app/models/datapool/itunes_store_app.rb
@@ -2,24 +2,21 @@
 #
 # Table name: datapool_store_products
 #
-#  id             :integer          not null, primary key
-#  type           :string(255)
-#  publisher_name :string(255)
-#  product_id     :string(255)      not null
-#  title          :string(255)      not null
-#  description    :text(65535)
-#  url            :string(255)      not null
-#  icon_url       :string(255)
-#  review_count   :integer          default(0), not null
-#  average_score  :float(24)        default(0.0), not null
-#  published_at   :datetime
-#  options        :text(65535)
+#  id            :integer          not null, primary key
+#  type          :string(255)
+#  genre         :integer          default(0), not null
+#  product_id    :string(255)      not null
+#  title         :string(255)      not null
+#  url           :string(255)      not null
+#  icon_url      :string(255)
+#  review_count  :integer          default(0), not null
+#  average_score :float(24)        default(0.0), not null
+#  options       :text(65535)
 #
 # Indexes
 #
-#  store_product_published_at_index  (published_at)
-#  store_product_unique_index        (product_id,type) UNIQUE
-#  store_product_url_index           (url)
+#  store_product_unique_index  (product_id,type) UNIQUE
+#  store_product_url_index     (url)
 #
 
 class Datapool::ItunesStoreApp < Datapool::StoreProduct
@@ -34,7 +31,7 @@ class Datapool::ItunesStoreApp < Datapool::StoreProduct
     top_free_pc: "https://rss.itunes.apple.com/api/v1/jp/macos-apps/top-free-mac-apps/all/300/explicit.json",
     top_grossing_pc: "https://rss.itunes.apple.com/api/v1/jp/macos-apps/top-grossing-mac-apps/all/300/explicit.json",
     top_all_pc: "https://rss.itunes.apple.com/api/v1/jp/macos-apps/top-mac-apps/all/300/explicit.json",
-    top_paid_pc: "https://rss.itunes.apple.com/api/v1/jp/macos-apps/top-paid-mac-apps/all/100/explicit.json",
+    top_paid_pc: "https://rss.itunes.apple.com/api/v1/jp/macos-apps/top-paid-mac-apps/all/300/explicit.json",
   }
 
   def self.update_rankings!

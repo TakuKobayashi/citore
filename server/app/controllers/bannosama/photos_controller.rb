45d0e67d7f2ce9a52b43b03d59bf01f4f0e4dbf9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-09-23 20:33:08 +0900

    S3にアップロードさせる

--- server/app/controllers/bannosama/photos_controller.rb
+++ server/app/controllers/bannosama/photos_controller.rb
@@ -5,7 +5,16 @@ class Bannosama::PhotosController < Bannosama::BaseController
   end
 
   def upload
+    greet = Bannosama::Greet.create(message: params[:say_comment].to_s, theme: params[:theme].to_i)
     upload_files = params[:image_files] || []
+    images = []
+    upload_files.each do |image_file|
+      greet_image = greet.images.new
+      greet_image.upload_s3_and_set_metadata(image_file)
+      images << greet_image
+    end
+    Bannosama::GreetImage.import(images)
+
     hash = params.dup.delete_if{|k, v| ["controller", "action"].include?(k) }
     render :json => {upload_file_count: upload_files.size, params: hash}
   end
--- server/app/models/bannosama/greet_image.rb
+++ server/app/models/bannosama/greet_image.rb
@@ -18,6 +18,20 @@
 #
 
 class Bannosama::GreetImage < ApplicationRecord
+  IMAGE_S3_FILE_ROOT = "project/bannosama/"
+
   serialize :options, JSON
   belongs_to :greet, class_name: 'Bannosama::Greet', foreign_key: :greet_id, required: false
+
+  def upload_s3_and_set_metadata(file)
+    fi = FastImage.new(file)
+    self.width = fi.size[0]
+    self.height = fi.size[1]
+    self.origin_file_name = file.original_filename
+    s3 = Aws::S3::Client.new
+    filename = SecureRandom.hex + File.extname(file.original_filename)
+    filepath = IMAGE_S3_FILE_ROOT + filename
+    s3.put_object(bucket: "taptappun",body: file.read, key: filepath, acl: "public-read")
+    self.upload_url = "https://taptappun.s3.amazonaws.com/" + filepath
+  end
 end

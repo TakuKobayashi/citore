b523e7c1a39af2b9d6968ba85ca877b19494f38b
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-09-24 09:29:37 +0900

    リサイズメソッド作成

--- server/app/models/bannosama/greet_image.rb
+++ server/app/models/bannosama/greet_image.rb
@@ -35,4 +35,16 @@ class Bannosama::GreetImage < ApplicationRecord
     s3.put_object(bucket: "taptappun",body: file.to_blob, key: filepath, acl: "public-read")
     self.upload_url = "https://taptappun.s3.amazonaws.com/" + filepath
   end
+
+  def self.calc_resize_text(width:, height:, max_length:)
+    if width > height
+      resized_width = [width, max_length].min
+      resized_height = ((resized_width.to_f / width.to_f) * height.to_f).to_i
+      return "#{resized_width.to_i}x#{resized_height.to_i}"
+    else
+      resized_height = [height, max_length].min
+      resized_width = ((resized_height.to_f / height.to_f) * width.to_f).to_i
+      return "#{resized_width.to_i}x#{resized_height.to_i}"
+    end
+  end
 end

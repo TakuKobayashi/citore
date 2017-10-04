c2aec14eb76a5c7db04dd3d8dfb89ab1ea3ebdc3
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-10-04 20:32:47 +0900

    タイポしていた

--- server/data/import_db.rb
+++ server/data/import_db.rb
@@ -10,9 +10,9 @@ files.each do |f|
     puts counter.to_s
     counter = counter + 1
     r.each do |row|
-      coulmns = row.split("\t")
+      columns = row.split("\t")
       begin
-        posted_at = Time.parse(coulmns[6])
+        posted_at = Time.parse(columns[6])
       rescue ArgumentError => ex
         puts f
         puts row
@@ -21,27 +21,27 @@ files.each do |f|
       next if posted_at.blank?
 
       arr << Datapool::HatsugenKomachi.new(
-        topic_id: coulmns[0],
-        res_number: coulmns[1],
-        top_res_flag: coulmns[2],
-        title: coulmns[3],
-        body: coulmns[4],
-        handle_name: coulmns[5],
+        topic_id: columns[0],
+        res_number: columns[1],
+        top_res_flag: columns[2],
+        title: columns[3],
+        body: columns[4],
+        handle_name: columns[5],
         posted_at: posted_at,
-        publish_flag: coulmns[7],
-        parent_topic_flag: coulmns[8],
-        komachi_user_id: coulmns[9],
-        advice: coulmns[10].to_s.strip,
-        funny: coulmns[11].to_i,
-        surprise: coulmns[12].to_i,
-        tears: coulmns[13].to_i,
-        yell: coulmns[14].to_i,
-        isee: coulmns[15].to_i,
-        genre_code: coulmns[16],
-        res_state: coulmns[17],
-        facemark_id: coulmns[18],
-        remark: coulmns[19].to_s.strip,
-        post_device: coulmns[20],
+        publish_flag: columns[7],
+        parent_topic_flag: columns[8],
+        komachi_user_id: columns[9],
+        advice: columns[10].to_s.strip,
+        funny: columns[11].to_i,
+        surprise: columns[12].to_i,
+        tears: columns[13].to_i,
+        yell: columns[14].to_i,
+        isee: columns[15].to_i,
+        genre_code: columns[16],
+        res_state: columns[17],
+        facemark_id: columns[18],
+        remark: columns[19].to_s.strip,
+        post_device: columns[20],
       )
     end
     Datapool::HatsugenKomachi.import!(arr)

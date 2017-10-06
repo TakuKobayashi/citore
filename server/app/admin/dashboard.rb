c6aa91987087ebe0b23faa62e0cf3132493352d4
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-10-06 23:35:49 +0900

    管理ツールからキューの状況を確認

--- server/app/admin/dashboard.rb
+++ server/app/admin/dashboard.rb
@@ -3,12 +3,14 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Sidekiqの状況" do
+          link_to("確認する", sidekiq_web_path)
+        end
       end
     end
+
 
     # Here is an example of a simple dashboard with columns and panels.
     #

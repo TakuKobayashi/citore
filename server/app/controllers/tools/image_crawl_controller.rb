0906acfb994cc19e76a07176417db33528c4f619
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-10-10 14:37:48 +0900

    リスタートできるように、一応全てのログを保存するようにする

--- server/app/controllers/tools/image_crawl_controller.rb
+++ server/app/controllers/tools/image_crawl_controller.rb
@@ -54,7 +54,7 @@ class Tools::ImageCrawlController < Homepage::BaseController
     if @upload_job.blank?
       @upload_job = @visitor.upload_jobs.find_or_initialize_by(token: params[:authenticity_token])
       @upload_job.from_type = prefix
-      @upload_job.options ||= {}
+      @upload_job.options ||= {params: params.to_h.dup}
       is_new = @upload_job.new_record?
       @upload_job.save!
     end

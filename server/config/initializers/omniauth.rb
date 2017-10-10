f94504b07bf0a63affcfcde3da5b502ac8410d82
TakuKobayshi <>
2017-10-10 11:42:35 +0900

    instagramも認証できるようにしておく

--- server/config/initializers/omniauth.rb
+++ server/config/initializers/omniauth.rb
@@ -3,4 +3,5 @@
 api_config = YAML.load(File.read("#{Rails.root.to_s}/config/apiconfig.yml"))
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, api_config["twitter"]["consumer_key"], api_config["twitter"]["consumer_secret"]
+  provider :instagram, api_config["instagram"]["client_id"], api_config["instagram"]["client_secret"], scope: 'basic+media+public_content+follower_list+comments+relationships+likes'
 end

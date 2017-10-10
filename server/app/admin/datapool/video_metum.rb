28299a11f4d90714ecd83ae9c98ec661e791bbb2
TakuKobayshi <>
2017-10-10 20:17:30 +0900

    ビデオは表紙と動画は分ける

--- server/app/admin/datapool/video_metum.rb
+++ server/app/admin/datapool/video_metum.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Datapool::VideoMetum do
   config.batch_actions = true
   config.per_page = 100
 
-  actions :index
+  actions :index, :show
 
   action_item(:crawl, only: :index) do
     link_to("クロールする", tools_image_crawl_path, class: "table_tools_button")
@@ -14,6 +14,20 @@ ActiveAdmin.register Datapool::VideoMetum do
     id_column
     column("タイトル") {|a| a.title }
     column("クラス名") {|a| a.type }
-    column("動画") {|a| video_tag(a.src, width: 200) }
+    column("動画の表紙画像") {|a| image_tag(a.front_image_url, width: 200) }
+  end
+
+  show do
+    panel datapool_video_metum.title do
+      attributes_table_for datapool_video_metum do
+        row("ID") {|a| a.id }
+        row("クラス名") {|a| a.type }
+        row("データ種別") {|a| a.data_category }
+        row("ビットレート") {|a| a.bitrate }
+        row("動画の表紙画像") {|a| image_tag(a.front_image_url) }
+        row("動画") {|a| video_tag(a.src) }
+        row("その他情報") {|a| a.options }
+      end
+    end
   end
 end

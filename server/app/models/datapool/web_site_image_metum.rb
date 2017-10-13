958fca995d15274af366d10a8badc012963add0f
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-10-13 21:32:59 +0900

    一旦コミット

--- server/app/models/datapool/web_site_image_metum.rb
+++ server/app/models/datapool/web_site_image_metum.rb
@@ -17,10 +17,14 @@
 #
 
 class Datapool::WebSiteImageMetum < Datapool::ImageMetum
-  def self.crawl_images!(url:, start_page: 1, end_page: 1, filter: nil, request_method: :get)
+  def self.crawl_images!(url:, page_key: nil, start_page: 1, end_page: 1, filter: nil, request_method: :get)
     images = []
     (start_page.to_i..end_page.to_i).each do |page|
-      address_url = Addressable::URI.parse(url.to_s % page.to_s)
+      address_url = Addressable::URI.parse(url.to_s)
+      if page_key.present?
+        queries = address_url.query_values || {}
+        address_url.query_values = queries.merge(page_key => page)
+      end
       break unless address_url.scheme.to_s.include?("http")
       doc = ApplicationRecord.request_and_parse_html(address_url.to_s, request_method)
       images += self.generate_objects_from_parsed_html(doc: doc, filter: filter, from_site_url: address_url.to_s)

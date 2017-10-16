fe5b74182e8a073a0033b5ba88b68062ad2b1ac4
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-10-17 03:43:48 +0900

    basic_sanitize

--- server/app/workers/image_crawl_worker.rb
+++ server/app/workers/image_crawl_worker.rb
@@ -5,7 +5,7 @@ class ImageCrawlWorker
     upload_job = Homepage::UploadJobQueue.find_by(id: upload_job_id)
     upload_job.crawling!
     if request_params["action"] == "url_crawl"
-      url = request_params["crawl_url"]
+      url = ApplicationRecord.basic_sanitize(request_params["crawl_url"].to_s)
       start_page = request_params["start_page_num"].to_i
       end_page = request_params["end_page_num"].to_i
       upload_job.options = upload_job.options.merge({url: url, start_page: start_page, end_page: end_page})
@@ -13,24 +13,27 @@ class ImageCrawlWorker
     elsif request_params["action"] == "flickr_crawl"
       search_type = request_params["search_type"].to_i
       search_hash = {}
+      keyword = ApplicationRecord.basic_sanitize(request_params["keyword"].to_s)
       if search_type == 1
-        search_hash[:tags] = request_params["keyword"].to_s
+        search_hash[:tags] = keyword
       else
-        search_hash[:text] = request_params["keyword"].to_s
+        search_hash[:text] = keyword
       end
-      upload_job.options = upload_job.options.merge({keyword: request_params["keyword"].to_s, search_type: search_type})
+      upload_job.options = upload_job.options.merge({keyword: keyword, search_type: search_type})
       images = Datapool::FrickrImageMetum.search_images!(search: search_hash)
     elsif request_params["action"] == "twitter_crawl"
+      keyword = ApplicationRecord.basic_sanitize(request_params["keyword"].to_s)
       search_type = request_params["search_type"].to_i
-      upload_job.options = upload_job.options.merge({keyword: request_params["keyword"].to_s, search_type: search_type})
+      upload_job.options = upload_job.options.merge({keyword: keyword, search_type: search_type})
       if search_type == 1
-        images = Datapool::TwitterImageMetum.images_from_user_timeline!(username: request_params["keyword"].to_s)
+        images = Datapool::TwitterImageMetum.images_from_user_timeline!(username: keyword)
       else
-        images = Datapool::TwitterImageMetum.search_image_tweet!(keyword: request_params["keyword"].to_s)
+        images = Datapool::TwitterImageMetum.search_image_tweet!(keyword: keyword)
       end
     else
-      upload_job.options = upload_job.options.merge({keyword: request_params["keyword"].to_s})
-      images = Datapool::GoogleImageSearch.crawl_images!(keyword: request_params["keyword"])
+      keyword = ApplicationRecord.basic_sanitize(request_params["keyword"].to_s)
+      upload_job.options = upload_job.options.merge({keyword: keyword})
+      images = Datapool::GoogleImageSearch.crawl_images!(keyword: keyword)
     end
     upload_job.save!
     if images.blank?

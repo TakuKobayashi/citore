6dc89d8738fd53620c0be175d1442c23d06af74c
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-10-17 01:15:50 +0900

    サニタイザー

--- server/app/models/datapool/web_site_image_metum.rb
+++ server/app/models/datapool/web_site_image_metum.rb
@@ -55,7 +55,7 @@ class Datapool::WebSiteImageMetum < Datapool::ImageMetum
       if title.blank?
         title = d.text
       end
-      image_url = Addressable::URI.parse(ApplicationRecord.basic_sanitize(d["src"].to_s.gsub(/(\r\n|\r|\n)/, "")))
+      image_url = Addressable::URI.parse(ApplicationRecord.basic_sanitize(ApplicationRecord.scan_url(d["src"].to_s).first.to_s))
       if image_url.nil?
         Rails.logger.warn("not exists image_url")
         next

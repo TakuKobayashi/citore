91e94565a1ad1792bf9457b515b6b0af7f6c3505
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-10-20 04:08:30 +0900

    こんな感じ?

--- server/app/models/datapool/web_site_audio_metum.rb
+++ server/app/models/datapool/web_site_audio_metum.rb
@@ -20,10 +20,42 @@
 class Datapool::WebSiteAudioMetum < Datapool::AudioMetum
   # audioタグがそんなにあるわけではないので、そのサイトにある音声ファイル全てを抑えにいく勢い
   def self.suppress!(url:)
-    target_urls = [url.to_s]
+    audios = []
+    audio_urls = []
+    target_urls = []
     crawl_urls = []
-    address_url = Addressable::URI.parse(url.to_s)
-    doc = ApplicationRecord.request_and_parse_html(address_url.to_s)
-    return []
+    root_crawl_url = Addressable::URI.parse(url.to_s)
+    crawl_url = root_crawl_url.origin
+    loop do
+      doc = ApplicationRecord.request_and_parse_html(crawl_url.to_s)
+      doc.css("a").each do |a|
+        link_url = Addressable::URI.parse(ApplicationRecord.merge_full_url(src: a["href"], org: root_crawl_url.to_s))
+        if root_crawl_url.host == link_url.host && !crawl_urls.include?(link_url.to_s)
+          crawl_urls << link_url.to_s
+          target_urls << link_url.to_s
+        end
+      end
+      doc.css("audio").each do |audio_doc|
+        audio_url = ApplicationRecord.merge_full_url(src: audio_doc["src"].to_s, org: root_crawl_url.to_s)
+        next if audio_urls.include?(audio_url.to_s)
+        audio_urls << audio_url.to_s
+        audio_metum = Datapool::WebSiteAudioMetum.new(
+          title: doc.title,
+          file_genre: :audio_file,
+          options: {root_from_url: url, from_url: crawl_url.to_s}
+        )
+        audio_metum.src = audio_url.to_s
+        audios << audio_metum
+      end
+      break if target_urls.blank?
+      crawl_url = target_urls.shift
+    end
+    audios.uniq!(&:src)
+    src_audios = Datapool::AudioMetum.find_origin_src_by_url(url: audios.map(&:src)).index_by(&:src)
+    import_audios = audios.select{|audio| src_audios[audio.src].blank? }
+    if import_audios.present?
+      Datapool::WebSiteAudioMetum.import!(import_audios)
+    end
+    return audios
   end
 end

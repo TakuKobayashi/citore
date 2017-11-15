74ea42ef1d50878b9e29b271c632024bbb21e0dc
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-11-15 10:19:46 +0900

    urlデバック確認用作成

--- server/app/admin/checked_link.rb
+++ server/app/admin/checked_link.rb
@@ -1,24 +1,22 @@
 ActiveAdmin.register_page "LinkChecker" do
+  menu priority: 1, label: "リンクチェック", parent: "debug"
+
   content title: "URL確認用" do
-    panel "エクセルまたはCSVのデータをインポート" do
-      active_admin_form_for(:register, url: admin_databatches_import_data_path) do |f|
-        f.inputs do
-          f.input :url, as: :text, label: "ファイルを選択"
-          f.input :before_truncate, as: :check_boxes, collection: ["Yes"], label: "インポートする前にデータベースを空にする?"
-          f.submit
-        end
+    active_admin_form_for(:register, url: admin_linkchecker_register_url) do |f|
+      f.inputs do
+        f.input :url, as: :string, label: "urlを入力する"
+        f.submit
       end
     end
 
     panel "link一覧" do
-      columns do
+      table do
+        th "url"
         urls = ExtraInfo.read_extra_info["check_link_urls"] || []
         urls.each do |url|
-          column do
-            link_to(url, url)
-          end
-          column do
-            link_to("削除する", url)
+          tr do
+            td link_to(url, url)
+            td link_to("削除する", admin_linkchecker_remove_url(url: url))
           end
         end
       end
@@ -26,10 +24,16 @@ ActiveAdmin.register_page "LinkChecker" do
   end
 
   page_action :register, method: :post do
-    redirect_to(active_admin_log_mst_imports_path, notice: "#{file.original_filename} importしました")
+    urls = ExtraInfo.read_extra_info["check_link_urls"] || []
+    urls << params[:register][:url]
+    ExtraInfo.update({"check_link_urls" => urls.compact.uniq})
+    redirect_to(admin_linkchecker_url, notice: "urlを追加しました")
   end
 
   page_action :remove, method: :get do
-    redirect_to(active_admin_log_mst_imports_path, notice: "アップロード開始")
+    urls = ExtraInfo.read_extra_info["check_link_urls"] || []
+    urls.reject!{|url| url == params[:url]}
+    ExtraInfo.update({"check_link_urls" => urls.compact.uniq})
+    redirect_to(admin_linkchecker_url, notice: "urlを削除しました")
   end
 end

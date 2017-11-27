9796206f325762ebaf25b3245655e025fdac6daf
TakuKobayshi <>
2017-11-27 17:13:23 +0900

    es6のものやバッククオートを含むとエラーが出るので...

--- server/app/assets/javascripts/libraries/threejs/editor/main.js
+++ server/app/assets/javascripts/libraries/threejs/editor/main.js
@@ -7,7 +7,7 @@ const url = require( 'url' );
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+var mainWindow;
 
 function createWindow() {
 

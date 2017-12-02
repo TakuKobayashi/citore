42364489e9dac07ee78880324b99304ffaafbc05
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-12-03 01:57:53 +0900

    対象の画像は真ん中切り抜いて正方形にする

--- server/app/models/hackathon/sunflower/image_resource.rb
+++ server/app/models/hackathon/sunflower/image_resource.rb
@@ -24,6 +24,9 @@ class Hackathon::Sunflower::ImageResource < ApplicationRecord
   has_many :worker_resources, class_name: 'Hackathon::Sunflower::WorkerResource', foreign_key: :resource_id
   has_many :workers, through: :worker_resources, source: :worker
 
+  BASE_IMAGE_WIDTH = 1378
+  BASE_IMAGE_HEIGHT = 1378
+
   IMAGE_ROOT_PATH = "hackathon/sunflower/images/"
 
   enum cateogory: {
@@ -40,6 +43,15 @@ class Hackathon::Sunflower::ImageResource < ApplicationRecord
   def upload!(file)
     image = MiniMagick::Image.open(file.path)
     image.format(:png)
+    if backgraound?
+      # 正方形になるように真ん中だけ切り抜く
+      if image.width < image.height
+        image.crop("#{image.width}x#{image.width}+0+#{(image.height - image.width) / 2}")
+      else
+        image.crop("#{image.height}x#{image.height}+#{(image.width - image.height) / 2}+0")
+      end
+      image.resize("#{BASE_IMAGE_WIDTH}x#{BASE_IMAGE_HEIGHT}")
+    end
     filepath = IMAGE_ROOT_PATH + SecureRandom.hex + ".png"
     s3 = Aws::S3::Client.new
     s3.put_object(bucket: "taptappun",body: image.to_blob, key: filepath, acl: "public-read")

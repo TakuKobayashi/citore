39c08c8795efd15bd1da9e03dfb3931deca219c6
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-12-02 13:04:44 +0900

    なぜか動いていた

--- server/config/initializers/omniauth.rb
+++ server/config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 
 api_config = YAML.load(File.read("#{Rails.root.to_s}/config/apiconfig.yml"))
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, api_config["twitter"]["consumer_key"], api_config["twitter"]["consumer_secret"]
+  provider :twitter, api_config["twitter"]["citore"]["consumer_key"], api_config["twitter"]["citore"]["consumer_secret"]
   provider :instagram, api_config["instagram"]["client_id"], api_config["instagram"]["client_secret"], scope: 'basic+media+public_content+follower_list+comments+relationships+likes'
   provider :spotify, api_config["spotify"]["client_id"], api_config["spotify"]["client_secret"], scope: 'playlist-read-private user-read-private user-read-email'
 end

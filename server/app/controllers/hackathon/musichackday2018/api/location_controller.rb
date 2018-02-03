0f19f8b3ecbb2ea9d1cc9c4ef1d95cbe109dc587
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-02-03 19:43:03 +0900

    サンプルのURLのJSONを返すようにする

--- server/app/controllers/hackathon/musichackday2018/api/location_controller.rb
+++ server/app/controllers/hackathon/musichackday2018/api/location_controller.rb
@@ -1,5 +1,18 @@
 class Hackathon::Musichackday2018::Api::LocationController < Hackathon::Musichackday2018::Api::BaseController
   def notify
-    render :layout => false, :json => params.dup
+    render :layout => false, :json => {
+      sender: {
+        lat: params[:lat],
+        lon: params[:lon]
+      },
+      timestamp: Time.current.to_i,
+      neighbours: [{
+        sound_url: "https://maoudamashii.jokersounds.com/music/bgm/mp3/bgm_maoudamashii_orchestra26.mp3",
+        distance: 100.0,
+        lat: 35.6598891,
+        lon: 139.7054377,
+        user_token: "aaaa"
+      }]
+    }
   end
 end

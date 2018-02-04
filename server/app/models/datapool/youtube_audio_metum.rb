3aa748d648bcdb4bc40561811f29502663932ccc
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-02-04 14:56:01 +0900

    wavで

--- server/app/models/datapool/youtube_audio_metum.rb
+++ server/app/models/datapool/youtube_audio_metum.rb
@@ -62,9 +62,9 @@ class Datapool::YoutubeAudioMetum < Datapool::AudioMetum
   end
 
   def download_and_upload_file!
-    file_name = SecureRandom.hex + ".m4a"
+    file_name = SecureRandom.hex + ".wav"
     output_file_path = Rails.root.to_s + "/tmp/audio/" + file_name
-    system("youtube-dl " + self.src + " -x -o " + output_file_path.to_s + " --audio-format=m4a")
+    system("youtube-dl " + self.src + " -x -o " + output_file_path.to_s + " --audio-format=wav")
     fileurl = Datapool::YoutubeAudioMetum.upload_s3(File.open(output_file_path), file_name)
     self.options["upload_audio_file_url"] = ApplicationRecord::S3_ROOT_URL + fileurl
     File.delete(output_file_path)

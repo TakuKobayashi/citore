48dbaea85ebe25eaf463ed61eaa3417ddfcb9305
TakuKobayshi <>
2018-05-08 16:16:03 +0900

    一旦取り消し

--- server/app/models/concerns/sanitizer.rb
+++ server/app/models/concerns/sanitizer.rb
@@ -58,7 +58,6 @@ module Sanitizer
   def self.basic_sanitize(text)
     #全角半角をいい感じに整える
     sanitized_word = Charwidth.normalize(text)
-    sanitized_word = sanitized_word.scrub
     #絵文字を除去
     sanitized_word = sanitized_word.encode('SJIS', 'UTF-8', invalid: :replace, undef: :replace, replace: '').encode('UTF-8')
     # 余分な空欄を除去

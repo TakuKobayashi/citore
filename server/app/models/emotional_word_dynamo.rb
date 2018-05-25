8dd682fd31db8deb9b82ea1d27badd862cd364d2
TakuKobayshi <>
2018-05-25 14:25:33 +0900

    dynamodbからテーブルを消すので、昔使ってたものをひとまず消す

--- server/app/models/emotional_word_dynamo.rb
+++
@@ -1,9 +0,0 @@
-class EmotionalWordDynamo
-  include Aws::Record
-
-  string_attr :word,  hash_key: true
-  string_attr :reading, range_key: true
-  string_attr :part, range_key: true
-  integer_attr :language
-  float_attr :score
-end
